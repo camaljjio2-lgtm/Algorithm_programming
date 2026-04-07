1c20082dd172b17966a7cfe71966882d7e70a685
Monkey_D_luffy <camaljjio2@gmail.com>
2026-04-07 19:59:16 +0900

    2차원 벡터를 활용한 그래프 구현

---
+++ Use_Vector_in_graph.cpp
@@ -0,0 +1,26 @@
+// 2차원 벡터를 사용한 그래프 구현 
+
+#include <iostream>
+#include <vector>
+
+struct Edge {
+    int endNode;
+    int value;
+};
+using namespace std;
+
+int main() {
+    vector<vector<Edge>> A;
+    int N, E;
+    cin >> N >> E;
+    A.resize(N+1);
+    //그냥 N이 아닌 이유는 대부분 자료구조 인덱스는 1부터 시작이기 떄문이다.
+    // 값을 넣는 반복문 
+    for (int i = 0; i < E; i++) {
+        int s,e,v = 0;
+        cin >> s >> e >> v;
+        A[s].push_back({e,v});
+    }
+    // 사용자가 원하는 값 불러오기 
+    
+}
