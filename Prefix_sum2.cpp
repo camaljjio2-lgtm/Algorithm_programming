cf834cace9efa23a2a2bf3a0c02fa7fd91d8094b
Monkey_D_luffy <camaljjio2@gmail.com>
2026-04-08 21:43:58 +0900

    구간 합 구하기 003-2

--- Prefix_sum2.cpp
+++ Prefix_sum2.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int grid[1025][1025] = {};
+    
     int N, M = 0;
     cin >> N >> M;
-
+    vector<vector<int>> grid(N+1, vector<int>(N+1,0));
     for (int i = 1 ; i <= N; i++) {
         for (int j = 1; j <=N; j++) {
             cin >> grid[i][j] ;
         }
     }
 
-    for (int j = 1; j < N; j++) {
-        for (int k = 1; k < N; k++) {
+    for (int j = 1; j <= N; j++) {
+        for (int k = 1; k <= N; k++) {
             grid[j][k] += grid[j][k-1] + grid[j-1][k] - grid[j-1][k-1];
             cout << grid[j][k] << " ";
         }
