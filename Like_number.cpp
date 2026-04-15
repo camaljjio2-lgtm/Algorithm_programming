545b3b9ffae1313838d72f7727eccac9a0743f37
Monkey_D_luffy <camaljjio2@gmail.com>
2026-04-15 19:09:13 +0900

    좋은 수

--- Like_number.cpp
+++ Like_number.cpp
@@ -3,6 +3,11 @@
 #include <algorithm>
 using namespace std;
 
+/*
+좋은 수 구하기
+N을 입력 받고 받은 크기 만큼 배열을 선언하고 값을 입력 한 후 다른 두수의 합으로 포현되는 식이 있다면 N개의 수가 몇개인지 말해봐라
+
+*/
 int main() 
 {
     ios::sync_with_stdio(false);
